6b3e60bef432adb77345514d1c54611271a50230
Teodor Pytka <teodorpytka@gmail.com>
2024-02-26 18:25:42 +0100

    feat: Generated catalog

---
+++ test/support/fixtures/catalog_fixtures.ex
@@ -0,0 +1,23 @@
+defmodule Ecom.CatalogFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Ecom.Catalog` context.
+  """
+
+  @doc """
+  Generate a product.
+  """
+  def product_fixture(attrs \\ %{}) do
+    {:ok, product} =
+      attrs
+      |> Enum.into(%{
+        description: "some description",
+        price: "120.5",
+        title: "some title",
+        views: 42
+      })
+      |> Ecom.Catalog.create_product()
+
+    product
+  end
+end
